cab91d131eb73e318064c73fbc9f78c966922d11
Aleksander <apropolos@gmail.com>
2025-11-27 19:07:21 +0100

    liczby pierwsze CU-86c6hxchf

--- prime.py
+++ prime.py
@@ -1,19 +1,22 @@
-
+# is_prime.py
 
 def is_prime(n: int) -> bool:
-
+    # liczby mniejsze niż 2 nie są pierwsze
     if not isinstance(n, int):
         return False
 
     if n < 2:
         return False
 
+    # 2 i 3 są pierwsze
     if n in (2, 3):
         return True
 
+    # liczby parzyste > 2 nie są pierwsze
     if n % 2 == 0:
         return False
 
+    # sprawdzamy dzielniki od 3 do sqrt(n), tylko nieparzyste
     limit = int(n ** 0.5) + 1
     for i in range(3, limit, 2):
         if n % i == 0:
